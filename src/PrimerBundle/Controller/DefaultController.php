3c4acf9f179b59c03f86974d53ace6fdeab6a396
Ekybio <jhot.256@gmail.com>
2017-11-16 12:12:16 -0500

    Primer Bundle

---
+++ src/PrimerBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PrimerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('PrimerBundle:Default:index.html.twig');
+    }
+}
